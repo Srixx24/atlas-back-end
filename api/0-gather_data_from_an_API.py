15d91c90b3c16cdc8e1c4f29db65d7d8d57e6fc3
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 16:23:42 -0700

    Edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,30 +4,40 @@ Retrieves employee data from a REST API and
 displays info on TODO list progress.
 """
 import requests
+import sys
 
 
 def get_employee_list(id):
     name_url = f"https://jsonplaceholder.typicode.com/users/{id}"
     todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
-    response = requests.get(name_url)
-    employee_data = response.json()
+    response_name = requests.get(name_url)
+    response_todo = requests.get(todo_url)
 
-    response = requests.get(todo_url)
-    todos = response.json()
+    if response_name.status_code == 200 and response_todo.status_code == 200:
+        employee_data = response_name.json()
+        todos = response_todo.json()
 
-    employee_name = employee_data['name']
-    total_tasks = len(todos)
-    completed_tasks = [todo['title'] for todo in todos if todo['completed']]
+        employee_name = employee_data['name']
+        total_tasks = len(todos)
+        completed_tasks = [todo['title'] for todo in todos if todo['completed']]
 
-    if response.status_code == 200:
         print(f"Employee {employee_name} is done with tasks "
               f"({len(completed_tasks)}/{total_tasks}):")
         print(f"EMPLOYEE_NAME:\t{employee_name}")
-        print(f"NUMBER_OF_DONE_TASKS:\t{completed_tasks}")
+        print(f"NUMBER_OF_DONE_TASKS:\t{len(completed_tasks)}")
         print(f"TOTAL_NUMBER_OF_TASKS:\t{total_tasks}")
         print("Completed Task Titles:")
         for title in completed_tasks:
             print(f"\t{title}")
     else:
         print("Failed to retrieve data.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: 0-gather_data_from_an_API.py <id>")
+        sys.exit(1)
+
+    id = sys.argv[1]
+    get_employee_list(id)
