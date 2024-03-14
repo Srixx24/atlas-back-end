9db3369604e0b909f0421a4c97027d46c735862a
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 09:20:47 -0700

    Edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,16 +5,18 @@ displays info on TODO list progress.
 """
 import requests
 
+
 def get_employee(id):
     url = f"https://jsonplaceholder.typicode.com/user/{id}/todo"
-    response =  requests.get(url)
+    response = requests.get(url)
     todos = response.json()
 
     employee_name = todos[0]['name']
     all_tasks = len(todos)
     completed = [todo['title'] for todo in todos if todo['completed']]
 
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks "
+          f"({completed_tasks}/{total_tasks}):")
     print(f"EMPLOYEE_NAME:\t{employee_name}")
     print(f"NUMBER_OF_DONE_TASKS:\t{completed_tasks}")
     print(f"TOTAL_NUMBER_OF_TASKS:\t{total_tasks}")
