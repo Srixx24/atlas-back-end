ab06355f80aa4039c2110a6f7ede44e8a4ffa311
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 09:42:58 -0700

    Edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,3 +23,6 @@ def get_employee(id):
     print("Completed Task Titles:")
     for title in completed_task_titles:
         print(f"\t{title}")
+
+id = int(input("Enter employee ID: "))
+get_employee(id)
