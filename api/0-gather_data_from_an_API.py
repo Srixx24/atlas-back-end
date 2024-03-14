380e38040a3d60c1a8b760444bddcaecdcad96b6
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 16:28:01 -0700

    Edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -27,7 +27,7 @@ def get_employee_list(id):
         ]
 
         print(f"Employee {employee_name} is done with tasks "
-              f"({len(completed_tasks)}/{total_tasks}):")
+              f"({completed_tasks}/{total_tasks}):")
         print(f"EMPLOYEE_NAME:\t{employee_name}")
         print(f"NUMBER_OF_DONE_TASKS:\t{len(completed_tasks)}")
         print(f"TOTAL_NUMBER_OF_TASKS:\t{total_tasks}")
