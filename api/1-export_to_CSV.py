6d632ed5e0e5431b093e270f490b1eedc9c52719
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 21:17:26 -0700

    Edits

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,32 +10,37 @@ import sys
 
 def export_csv_data(id):
     name_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
     response_name = requests.get(name_url)
+    response_todo = requests.get(todo_url)
 
-    if response_name.status_code == 200:
+    if response_name.status_code == 200 and response_todo.status_code == 200:
         employee_data = response_name.json()
+        todos = response_todo.json()
 
         employee_name = employee_data['username']
         employee_id = employee_data['id']
 
-        tasks = [{
+        tasks = [
+            {
                 "USER_ID": employee_id,
                 "USERNAME": employee_name,
-                "TASK_COMPLETED_STATUS": "completed",
-                "TASK_TITLE": "title"
-            }]
+                "TASK_COMPLETED_STATUS": todo['completed'],
+                "TASK_TITLE": todo['title']
+            }
+            for todo in todos
+        ]
 
         # Start of csv file
-        filename = "USER_ID.csv"
-        fields = [
+        filename = f"USER_{id}.csv"
+        with open(filename, 'w', newline='') as csvfile:
+            fields = [
                 'USER_ID',
                 'USERNAME',
                 'TASK_COMPLETED_STATUS',
                 'TASK_TITLE'
                 ]
-
-        with open(filename, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fields)
             writer.writeheader()
             writer.writerows(tasks)
