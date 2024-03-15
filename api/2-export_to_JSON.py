d59695d59e446a77f83d394ca8248cf5547a7f37
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-14 21:23:55 -0700

    Edits

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Retrieves employee data from a REST API
+in JSON format and displays info.
+"""
+import json
+import requests
+import sys
+
+
+def export_json_data(id):
+    name_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+
+    response_name = requests.get(name_url)
+    response_todo = requests.get(todo_url)
+
+    if response_name.status_code == 200 and response_todo.status_code == 200:
+        employee_data = response_name.json()
+        todos = response_todo.json()
+
+        employee_name = employee_data['username']
+        employee_id = employee_data['id']
