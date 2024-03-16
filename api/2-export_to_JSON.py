e451552bf031ef9c84d73cba0eccb5d6eacab4b1
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-15 19:46:03 -0700

    Edits

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,7 +8,29 @@ import requests
 import sys
 
 
-def export_json_data(id):
+def export_json_data(tasks, user_id, employee_name):
+    filename = f"{user_id}.json"
+
+    tasks = {
+        str(user_id): [ 
+            {
+                "task": task['title'],
+                "completed": task['completed'], 
+                "username": employee_name
+            }
+            for task in tasks
+        ]
+    }
+
+    json_object = json.dumps(tasks, indent=4)
+
+    with open(filename, "w") as json_file:
+        json_file.write(json_object)
+
+print(f"Data has been written to user_id.json")
+
+
+def get_employee_list(id):
     name_url = f"https://jsonplaceholder.typicode.com/users/{id}"
     todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
@@ -17,7 +39,18 @@ def export_json_data(id):
 
     if response_name.status_code == 200 and response_todo.status_code == 200:
         employee_data = response_name.json()
-        todos = response_todo.json()
+        tasks = response_todo.json()
 
-        employee_name = employee_data['username']
-        employee_id = employee_data['id']
+        employee_name = employee_data['name']
+        export_json_data(id, employee_name, tasks)
+    else:
+        print("Failed to retrieve data.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: 2-export_to_JSON.py <id>")
+        sys.exit(1)
+
+    id = sys.argv[1]
+    get_employee_list(id)
